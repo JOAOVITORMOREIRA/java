05b433503be46404b66816654bfb86f8e50a31c1
João Vitor Moreira <joaoitagv@gmail.com>
2023-03-22 19:59:15 -0300

    Atividade Collections

---
+++ exercicios_05/src/exercicios_05/ExercicioCollectionList.java
@@ -0,0 +1,37 @@
+package exercicios_05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExercicioCollectionList {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+	        numeros.add(2);
+	        numeros.add(5);
+	        numeros.add(1);
+	        numeros.add(3);
+	        numeros.add(4);
+	        numeros.add(9);
+	        numeros.add(7);
+	        numeros.add(8);
+	        numeros.add(10);
+	        numeros.add(6);
+
+	        
+	        System.out.println("Digite um número : ");
+	        int numero = scanner.nextInt();
+	        int posicao = numeros.indexOf(numero);
+
+	        if (posicao >= 0) {
+	            System.out.println("O número " + numero + " está na posição " + posicao + " da lista.");
+	        } else {
+	            System.out.println("O número " + numero + " não foi encontrado!");
+	        }
+	    }
+
+	}
